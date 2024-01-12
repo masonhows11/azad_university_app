df062550664f9bac4cb980ad7bb2d489bcee3a77
mason.hows11 <mason.hows11@gmail.com>
2024-01-12 08:54:16 +0330

    make master blade

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends Controller
 
     public function home()
     {
-
         return view('home');
     }
 }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,1 +1,350 @@
-<?php
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css"
+          href="bootstrap/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="stylesheet" href="style/font.css">
+    <title>Azad university</title>
+</head>
+<body>
+
+<div class="navbar-section">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">دانشگاه آزاد اسلامی</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">خانه</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">اساتید</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">دانشجو</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+
+<div class="search-section">
+    <div class="container">
+
+        <form class>
+            <div class="row">
+
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>نمایش بر اساس رشته تحصیلی</option>
+                        <option value="1">رشته معماری</option>
+                        <option value="2">رشته عمران</option>
+                        <option value="3">رشته راه و ساختمان</option>
+                        <option value="3">رشته کامپیوتر نرم افزار</option>
+                    </select>
+                </div>
+
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <button type="submit" class="btn btn-primary w-100">جستجو</button>
+                </div>
+
+            </div>
+        </form>
+
+    </div>
+</div>
+
+<div class="main-section">
+    <div class="container">
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+
+            <div class="col">
+                <div class="card item-master-info">
+
+                    <div class="card-body item-master-image">
+                        <div>
+                            <div class="image img-thumbnail">
+                                <a href="single.html"><img src="images/no-image-icon.png"
+                                                           class="img-thumbnail" alt="master-image"></a>
+                            </div>
+                            <a href="single.html"><h5
+                                    class="card-title item-master-name">دکتر فریبا
+                                    کریمی</h5></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="master-info-list">
+                            <li>مرتبه علمی : <span
+                                    class="master-info-value">دانشیار</span></li>
+                            <li>رشته تحصیلی : <span class="master-info-value">مدیریت
+                      آموزشی</span></li>
+                            <li>گروه آموزشی : <span class="master-info-value">علوم
+                      تربیتی</span></li>
+
+                            <li>دانشکده : <span class="master-info-value">علوم تربیتی،
+                      روانشناسی و زبان های خارجی</span></li>
+                            <li>بازدید : <span class="master-info-value">27071</span></li>
+                            <li>آدرس : <span class="master-info-value"><a
+                                        class="master-web-site"
+                                        href="https:\\prof.khuisf.ac.ir/f.karimi">prof.khuisf.ac.ir/f.karimi</a></span></li>
+                        </ul>
+                        <a href="#" class="master-talking-room" style="color:white"><i
+                                class="fas fa-comments"></i> ورود به اتاق گفتگوی مجازی </a>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card item-master-info">
+
+                    <div class="card-body item-master-image">
+                        <div>
+                            <div class="image img-thumbnail">
+                                <a href="single.html"><img src="images/no-image-icon.png"
+                                                           class="img-thumbnail" alt="master-image"></a>
+                            </div>
+                            <a href="single.html"><h5
+                                    class="card-title item-master-name">دکتر فریبا
+                                    کریمی</h5></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="master-info-list">
+                            <li>مرتبه علمی : <span
+                                    class="master-info-value">دانشیار</span></li>
+                            <li>رشته تحصیلی : <span class="master-info-value">مدیریت
+                      آموزشی</span></li>
+                            <li>گروه آموزشی : <span class="master-info-value">علوم
+                      تربیتی</span></li>
+
+                            <li>دانشکده : <span class="master-info-value">علوم تربیتی،
+                      روانشناسی و زبان های خارجی</span></li>
+                            <li>بازدید : <span class="master-info-value">27071</span></li>
+                            <li>آدرس : <span class="master-info-value"><a
+                                        class="master-web-site"
+                                        href="https:\\prof.khuisf.ac.ir/f.karimi">prof.khuisf.ac.ir/f.karimi</a></span></li>
+                        </ul>
+                        <a href="#" class="master-talking-room" style="color:white"><i
+                                class="fas fa-comments"></i> ورود به اتاق گفتگوی مجازی </a>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card item-master-info">
+
+                    <div class="card-body item-master-image">
+                        <div>
+                            <div class="image img-thumbnail">
+                                <a href="single.html"><img src="images/no-image-icon.png"
+                                                           class="img-thumbnail" alt="master-image"></a>
+                            </div>
+                            <a href="single.html"><h5
+                                    class="card-title item-master-name">دکتر فریبا
+                                    کریمی</h5></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="master-info-list">
+                            <li>مرتبه علمی : <span
+                                    class="master-info-value">دانشیار</span></li>
+                            <li>رشته تحصیلی : <span class="master-info-value">مدیریت
+                      آموزشی</span></li>
+                            <li>گروه آموزشی : <span class="master-info-value">علوم
+                      تربیتی</span></li>
+
+                            <li>دانشکده : <span class="master-info-value">علوم تربیتی،
+                      روانشناسی و زبان های خارجی</span></li>
+                            <li>بازدید : <span class="master-info-value">27071</span></li>
+                            <li>آدرس : <span class="master-info-value"><a
+                                        class="master-web-site"
+                                        href="https:\\prof.khuisf.ac.ir/f.karimi">prof.khuisf.ac.ir/f.karimi</a></span></li>
+                        </ul>
+                        <a href="#" class="master-talking-room" style="color:white"><i
+                                class="fas fa-comments"></i> ورود به اتاق گفتگوی مجازی </a>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card item-master-info">
+
+                    <div class="card-body item-master-image">
+                        <div>
+                            <div class="image img-thumbnail">
+                                <a href="single.html"><img src="images/no-image-icon.png"
+                                                           class="img-thumbnail" alt="master-image"></a>
+                            </div>
+                            <a href="single.html"><h5
+                                    class="card-title item-master-name">دکتر فریبا
+                                    کریمی</h5></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="master-info-list">
+                            <li>مرتبه علمی : <span
+                                    class="master-info-value">دانشیار</span></li>
+                            <li>رشته تحصیلی : <span class="master-info-value">مدیریت
+                      آموزشی</span></li>
+                            <li>گروه آموزشی : <span class="master-info-value">علوم
+                      تربیتی</span></li>
+
+                            <li>دانشکده : <span class="master-info-value">علوم تربیتی،
+                      روانشناسی و زبان های خارجی</span></li>
+                            <li>بازدید : <span class="master-info-value">27071</span></li>
+                            <li>آدرس : <span class="master-info-value"><a
+                                        class="master-web-site"
+                                        href="https:\\prof.khuisf.ac.ir/f.karimi">prof.khuisf.ac.ir/f.karimi</a></span></li>
+                        </ul>
+                        <a href="#" class="master-talking-room" style="color:white"><i
+                                class="fas fa-comments"></i> ورود به اتاق گفتگوی مجازی </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card item-master-info">
+
+                    <div class="card-body item-master-image">
+                        <div>
+                            <div class="image img-thumbnail">
+                                <a href="single.html"><img src="images/no-image-icon.png"
+                                                           class="img-thumbnail" alt="master-image"></a>
+                            </div>
+                            <a href="single.html"><h5
+                                    class="card-title item-master-name">دکتر فریبا
+                                    کریمی</h5></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="master-info-list">
+                            <li>مرتبه علمی : <span
+                                    class="master-info-value">دانشیار</span></li>
+                            <li>رشته تحصیلی : <span class="master-info-value">مدیریت
+                      آموزشی</span></li>
+                            <li>گروه آموزشی : <span class="master-info-value">علوم
+                      تربیتی</span></li>
+
+                            <li>دانشکده : <span class="master-info-value">علوم تربیتی،
+                      روانشناسی و زبان های خارجی</span></li>
+                            <li>بازدید : <span class="master-info-value">27071</span></li>
+                            <li>آدرس : <span class="master-info-value"><a
+                                        class="master-web-site"
+                                        href="https:\\prof.khuisf.ac.ir/f.karimi">prof.khuisf.ac.ir/f.karimi</a></span></li>
+                        </ul>
+                        <a href="#" class="master-talking-room" style="color:white"><i
+                                class="fas fa-comments"></i> ورود به اتاق گفتگوی مجازی </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card item-master-info">
+
+                    <div class="card-body item-master-image">
+                        <div>
+                            <div class="image img-thumbnail">
+                                <a href="single.html"><img src="images/no-image-icon.png"
+                                                           class="img-thumbnail" alt="master-image"></a>
+                            </div>
+                            <a href="single.html"><h5
+                                    class="card-title item-master-name">دکتر فریبا
+                                    کریمی</h5></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="master-info-list">
+                            <li>مرتبه علمی : <span
+                                    class="master-info-value">دانشیار</span></li>
+                            <li>رشته تحصیلی : <span class="master-info-value">مدیریت
+                      آموزشی</span></li>
+                            <li>گروه آموزشی : <span class="master-info-value">علوم
+                      تربیتی</span></li>
+
+                            <li>دانشکده : <span class="master-info-value">علوم تربیتی،
+                      روانشناسی و زبان های خارجی</span></li>
+                            <li>بازدید : <span class="master-info-value">27071</span></li>
+                            <li>آدرس : <span class="master-info-value"><a
+                                        class="master-web-site"
+                                        href="https:\\prof.khuisf.ac.ir/f.karimi">prof.khuisf.ac.ir/f.karimi</a></span></li>
+                        </ul>
+                        <a href="#" class="master-talking-room" style="color:white"><i
+                                class="fas fa-comments"></i> ورود به اتاق گفتگوی مجازی </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+<div class="footer-section">
+    <div class="footer-content">
+        <div class="container-fluid">
+            <div class="row">
+
+                <div class="footer-section-right col-lg-3 col-md-4 col-sm-4">
+
+                    <div class="footer-right-content ">
+
+                    </div>
+
+                </div>
+
+                <div class="footer-section-center col-lg-3 col-md-4 col-sm-4">
+
+                    <div class="footer-center-content ">
+
+                    </div>
+
+                </div>
+
+                <div class="footer-section-center col-lg-3 col-md-4 col-sm-4">
+
+                    <div class="footer-center-content ">
+
+                    </div>
+
+                </div>
+
+                <div class="footer-section-left col-lg-3 col-md-4 col-sm-4">
+
+                    <div class="footer-left-content  ">
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="bootstrap/js/jquery-3.5.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
